5add651592702eda6b100010351ab2fb0aefc0f3
Nuno Marques <ositaka@gmail.com>
2021-03-28 10:47:40 +0100

    fix

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,5 +1,5 @@
 /* eslint-disable */
-const static = require('@sveltejs/adapter-static');
+const adapter = require('@sveltejs/adapter-static');
 const pkg = require('./package.json');
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,7 @@ module.exports = {
 		// By default, `npm run build` will create a standard static app.
 		// You can create optimized builds for different platforms by
 		// specifying a different adapter
-		adapter: static(),
+		adapter: adapter(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
